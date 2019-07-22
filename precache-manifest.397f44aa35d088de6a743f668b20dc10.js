87c4e990b81810bff7e4d8537f416f87f21aae2b
Marko Djordjevic <makezi08@gmail.com>
2019-07-22 13:30:51 +1000

    Updates

---
+++ precache-manifest.397f44aa35d088de6a743f668b20dc10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c712e8b45b26b7129a6f3cb3e13dd1e3",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "87a9f00b204377746a80",
+    "url": "/react-todolist/static/css/main.c2cbd366.chunk.css"
+  },
+  {
+    "revision": "9ed43af723460b350941",
+    "url": "/react-todolist/static/js/2.8181e864.chunk.js"
+  },
+  {
+    "revision": "87a9f00b204377746a80",
+    "url": "/react-todolist/static/js/main.3aa1f725.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
