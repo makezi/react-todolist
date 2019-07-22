60bbfa389d13bddfe1e5d364dbb69baf917a1441
Marko Djordjevic <makezi08@gmail.com>
2019-07-22 12:57:37 +1000

    Clean up TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,23 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
-import TodoItem from "../TodoItem/TodoItem";
-import { VisibilityFilters } from "../../constants";
-import "./TodoList.css";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function filterTodos(todos, filter) {
-  console.log("filter", filter);
-  switch (filter) {
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(todo => todo.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.completed);
-    case VisibilityFilters.SHOW_ALL:
-    default:
-      return todos;
-  }
-}
+import TodoItem from '../TodoItem/TodoItem';
+import { VisibilityFilters } from '../../constants';
+import './TodoList.css';
 
 const TodoList = ({ todos, filter, deleteTodo, completeTodo }) => {
+  const filterTodos = (todos, filter) => {
+    switch (filter) {
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter(todo => todo.completed);
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter(todo => !todo.completed);
+      case VisibilityFilters.SHOW_ALL:
+      default:
+        return todos;
+    }
+  };
+
   return (
     <ul className="todo-list">
       {filterTodos(todos, filter).map(todo => (
@@ -33,7 +33,14 @@ const TodoList = ({ todos, filter, deleteTodo, completeTodo }) => {
 };
 
 TodoList.propTypes = {
-  todos: PropTypes.array.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      todo: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    })
+  ).isRequired,
+  filter: PropTypes.string.isRequired,
   deleteTodo: PropTypes.func.isRequired,
   completeTodo: PropTypes.func.isRequired
 };
