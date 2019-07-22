c567ffa5e6a59e57074c867f34bbb0ac79b23cb0
Marko Djordjevic <makezi08@gmail.com>
2019-07-22 12:51:58 +1000

    Refactor AddTodoInput to use hooks

--- src/components/AddTodoInput/AddTodoInput.js
+++ src/components/AddTodoInput/AddTodoInput.js
@@ -1,49 +1,43 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import "./AddTodoInput.css";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-class AddTodoInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { todo: "" };
-    this.handleInput = this.handleInput.bind(this);
-    this.addTodo = this.addTodo.bind(this);
-  }
+import './AddTodoInput.css';
 
-  handleInput(e) {
-    if (e.target.value.length > 40) return;
-    this.setState({ todo: e.target.value });
-  }
+const AddTodoInput = ({ addTodo }) => {
+  const [todo, setTodo] = useState('');
 
-  addTodo(e) {
-    e.preventDefault();
-    this.props.addTodo(this.state.todo);
-    this.setState({ todo: "" });
-  }
+  const handleOnInputChange = event => {
+    if (event.target.value.length > 40) return;
+    setTodo(event.target.value);
+  };
 
-  render() {
-    return (
-      <form className="add-todo" onSubmit={this.addTodo}>
-        <input
-          className="add-todo--text"
-          type="text"
-          placeholder="What needs to be done?"
-          value={this.state.todo}
-          onChange={this.handleInput}
-        />
-        <input
-          className="add-todo--submit"
-          type="submit"
-          value="+"
-          onChange={this.handleInput}
-        />
-      </form>
-    );
-  }
-}
+  const addTodoFromInput = event => {
+    event.preventDefault();
+    addTodo(todo);
+    setTodo('');
+  };
+
+  return (
+    <form className="add-todo" onSubmit={addTodoFromInput}>
+      <input
+        className="add-todo--text"
+        type="text"
+        placeholder="What needs to be done?"
+        value={todo}
+        onChange={handleOnInputChange}
+      />
+      <input
+        className="add-todo--submit"
+        type="submit"
+        value="+"
+        onChange={handleOnInputChange}
+      />
+    </form>
+  );
+};
 
 AddTodoInput.propTypes = {
-  todo: PropTypes.object
+  addTodo: PropTypes.func.isRequired
 };
 
 export default AddTodoInput;
