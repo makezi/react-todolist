3c1f5598fc4f0f5ee85725470b161e4890385009
Marko Djordjevic <makezi08@gmail.com>
2019-07-22 13:06:14 +1000

    Add "deleteTodo" prop type definition to TodoItem

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,29 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./TodoItem.css";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const TodoItem = ({ todo, deleteTodo, completeTodo }) => {
-  return (
-    <div className="todo-item">
-      <input
-        type="checkbox"
-        id={todo.id}
-        defaultChecked={todo.completed}
-        onClick={() => completeTodo(todo.id)}
-      />
-      <label htmlFor={todo.id}>
-        <div className="todo-item--checkbox" />
-        {todo.todo}
-      </label>
-      <button onClick={() => deleteTodo(todo.id)}>
-        <i className="fas fa-trash" />
-      </button>
-    </div>
-  );
-};
+import './TodoItem.css';
+
+const TodoItem = ({ todo, deleteTodo, completeTodo }) => (
+  <div className="todo-item">
+    <input
+      type="checkbox"
+      id={todo.id}
+      defaultChecked={todo.completed}
+      onClick={() => completeTodo(todo.id)}
+    />
+    <label htmlFor={todo.id}>
+      <div className="todo-item--checkbox" />
+      {todo.todo}
+    </label>
+    <button onClick={() => deleteTodo(todo.id)}>
+      <i className="fas fa-trash" />
+    </button>
+  </div>
+);
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
   completeTodo: PropTypes.func.isRequired
 };
 
