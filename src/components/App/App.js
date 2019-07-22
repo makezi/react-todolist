0fbd29b0e2bc9688d821e068a80f53e1dfc91d0d
Marko Djordjevic <makezi08@gmail.com>
2019-07-22 12:44:09 +1000

    Refactor App to use hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,92 +1,61 @@
-import React, { Component } from "react";
-import uuidv1 from "uuid";
-import AddTodoInput from "../AddTodoInput/AddTodoInput";
-import TodoList from "../TodoList/TodoList";
-import FilterTabs from "../FilterTabs/FilterTabs";
-import TodosRemainingText from "../TodosRemainingText/TodosRemainingText";
-import { VisibilityFilters } from "../../constants";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import uuidv1 from 'uuid';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: JSON.parse(localStorage.getItem("todos")) || [
-        { id: uuidv1(), todo: "Practice guitar", completed: true },
-        { id: uuidv1(), todo: "Learn React", completed: false }
-      ],
-      filter: VisibilityFilters.SHOW_ALL
-    };
-    this.addTodo = this.addTodo.bind(this);
-    this.deleteTodo = this.deleteTodo.bind(this);
-    this.completeTodo = this.completeTodo.bind(this);
-    this.clearCompleted = this.clearCompleted.bind(this);
-    this.calculateRemainingTodos = this.calculateRemainingTodos.bind(this);
-    this.setFilter = this.setFilter.bind(this);
-  }
+import AddTodoInput from '../AddTodoInput/AddTodoInput';
+import TodoList from '../TodoList/TodoList';
+import FilterTabs from '../FilterTabs/FilterTabs';
+import TodosRemainingText from '../TodosRemainingText/TodosRemainingText';
+import { VisibilityFilters } from '../../constants';
+import './App.css';
 
-  componentDidUpdate() {
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
-  }
+const INITIAL_TODOS = JSON.parse(localStorage.getItem('todos')) || [
+  { id: uuidv1(), todo: 'Practice guitar', completed: true },
+  { id: uuidv1(), todo: 'Learn React', completed: false }
+];
 
-  addTodo(todo) {
-    console.log(todo);
-    if (!todo) return;
-    this.setState({
-      todos: [{ id: uuidv1(), todo, completed: false }, ...this.state.todos]
-    });
-  }
+const App = () => {
+  const [todos, setTodos] = useState(INITIAL_TODOS);
+  const [filter, setFilter] = useState(VisibilityFilters.SHOW_ALL);
 
-  deleteTodo(id) {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    });
-  }
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-  completeTodo(id) {
-    this.setState({
-      todos: this.state.todos.map(
-        todo =>
-          todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    });
-  }
+  const addTodo = todo => {
+    const newTodos = [{ id: uuidv1(), todo, completed: false }, ...todos];
+    setTodos(newTodos);
+  };
 
-  clearCompleted() {
-    this.setState({
-      todos: this.state.todos.filter(todo => !todo.completed)
-    });
-  }
+  const deleteTodo = id => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
 
-  calculateRemainingTodos() {
-    const todosLeft = this.state.todos.reduce((total, todo) => {
-      return total + (todo.completed ? 0 : 1);
-    }, 0);
-    return todosLeft;
-  }
-
-  setFilter(filter) {
-    console.log(filter);
-    this.setState({ filter });
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <AddTodoInput addTodo={this.addTodo} />
-        <TodoList
-          todos={this.state.todos}
-          filter={this.state.filter}
-          deleteTodo={this.deleteTodo}
-          completeTodo={this.completeTodo}
-        />
-        <div className="info">
-          <TodosRemainingText todosLeft={this.calculateRemainingTodos()} />
-        </div>
-        <FilterTabs setFilter={this.setFilter} />
-      </div>
+  const completeTodo = id => {
+    const newTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
-  }
-}
+    setTodos(newTodos);
+  };
+
+  const calculateRemainingTodos = () =>
+    todos.reduce((total, todo) => (total + todo.completed ? 0 : 1), 0);
+
+  return (
+    <div className="app">
+      <AddTodoInput addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        filter={filter}
+        deleteTodo={deleteTodo}
+        completeTodo={completeTodo}
+      />
+      <div className="info">
+        <TodosRemainingText todosLeft={calculateRemainingTodos} />
+      </div>
+      <FilterTabs setFilter={setFilter} />
+    </div>
+  );
+};
 
 export default App;
